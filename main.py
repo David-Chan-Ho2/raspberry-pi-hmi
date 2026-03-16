53ecf172a2d70bbcb98dd6526cbc492aa71761c4
David-Chan-Ho2 <dho12@charlotte.edu>
2026-03-16 09:54:21 -0400

    add logging

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import sys
-
+import logging
+import traceback
 from PySide6.QtWidgets import QApplication
 
 from ui.main_window import MainWindow
 
 app = QApplication(sys.argv)
 
-# with open("styles/styles.qss", "r") as f:
+logging.basicConfig(
+    filename='/home/dho/hmi_debug.log',
+    level=logging.ERROR,
+    format='%(asctime)s %(levelname)s:%(message)s'
+)
+
+# with open("/home/dho/styles/styles.qss", "r") as f:
 #     app.setStyleSheet(f.read())
 
-window = MainWindow()
-window.show()
+try:
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+except Exception as e:
+    logging.error("HMI Crashed!")
+    logging.error(traceback.format_exc())
+    sys.exit(1)
 
-sys.exit(app.exec())
